597370ad437f3310bdb20a33b292560c075355af
Smarty <fluf95@gmail.com>
2017-03-09 11:59:42 +0000

    Initial Commit

---
+++ Assets/Scripts/RoomCheckerScript.cs
@@ -0,0 +1,189 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomCheckerScript : MonoBehaviour {
+
+
+
+	public  CattributesScript cattributes;
+     public MobileInputScript InputScript;
+
+    
+   
+    bool CatPickedUp;
+	public GameObject CatLock;
+	bool CatInPlace;
+
+	// Use this for initialization
+	void Start () {
+
+		cattributes = this.gameObject.GetComponent ("CattributesScript") as CattributesScript;
+		InputScript = this.gameObject.GetComponent ("MobileInputScript") as MobileInputScript;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CatPickedUp = InputScript.CatPickedUp;
+
+//		if (this.gameObject.transform.position != CatLock.transform.position) 
+//		{
+//			CatInPlace = false;
+//		}
+//
+
+//		if (CatLock != null && InputScript.CatPickedUp == false && CatInPlace == false) 
+//		{
+//			
+//			LockCat ();
+//
+//
+//		}
+
+
+
+
+
+	}
+
+
+//	void OnTriggerStay (Collider other)
+//	{
+//		if (other.gameObject.tag == "CatLock" ) 
+//		{
+//			CatLock = other.gameObject;
+//			if (InputScript.CatPickedUp == false) 
+//			{
+//				this.gameObject.transform.position = CatLock.transform.position;
+//			}
+//		}
+//
+//
+//
+//	}
+
+
+	void LockCat()
+	{
+		this.gameObject.transform.position = CatLock.transform.position;
+		CatInPlace = true;
+	}
+
+
+    void OnTriggerEnter(Collider other)
+    {
+		Debug.Log ("Entered Room");
+		CatLock = other.gameObject.transform.Find ("CatLock").gameObject;
+		LockCat ();
+	
+
+            if (other.gameObject.tag == "GymLevel1")
+            {
+                
+                cattributes.GymLevel1();
+                Debug.Log("Cat placed in Gym");
+                
+
+		}
+
+
+            if (other.gameObject.tag == "GymLevel2")
+            {
+			
+
+			Debug.Log ("Gym");
+			cattributes.GymLevel2 ();
+                Debug.Log("Cat placed in Gym");
+                
+
+
+            }
+
+
+
+            if (other.gameObject.tag == "GymLevel3")
+            {
+                cattributes.GymLevel3();
+                Debug.Log("Cat placed in Gym");
+            }
+
+
+
+
+
+
+
+            if (other.gameObject.tag == "TreadMillLevel1")
+            {
+                cattributes.TreadMillLevel1();
+                Debug.Log("Cat placed on the TreadMill");
+            }
+
+
+            if (other.gameObject.tag == "TreadMillLevel2")
+            {
+                cattributes.TreadMillLevel2();
+                Debug.Log("Cat placed on the TreadMill");
+            }
+
+            if (other.gameObject.tag == "TreadMillLevel3")
+            {
+                cattributes.TreadMillLevel3();
+                Debug.Log("Cat placed on the TreadMill");
+            }
+
+
+
+            if (other.gameObject.tag == "LaserRoomLevel1")
+            {
+                cattributes.ArcadeLevel1();
+                Debug.Log("Cat placed in LaserRoom");
+            }
+
+            if (other.gameObject.tag == "LaserRoomLevel2")
+            {
+                cattributes.ArcadeLevel2();
+                Debug.Log("Cat placed in LaserRoom");
+            }
+
+            if (other.gameObject.tag == "LaserRoomLevel3")
+            {
+                cattributes.ArcadeLevel3();
+                Debug.Log("Cat placed in LaserRoom");
+            }
+
+
+
+
+
+            if (other.gameObject.tag == "KitchenLevel1")
+            {
+                cattributes.KitchenLevel1();
+                Debug.Log("Cat placed in Kitchen");
+            }
+
+
+            if (other.gameObject.tag == "KitchenLevel2")
+            {
+                cattributes.KitchenLevel2();
+                Debug.Log("Cat placed in Kitchen");
+            }
+
+
+            if (other.gameObject.tag == "KitchenLevel3")
+            {
+                cattributes.KitchenLevel3();
+                Debug.Log("Cat placed in Kitchen");
+            }
+
+        
+    }
+
+	void OnTriggerExit (Collider other)
+	{
+		cattributes.InActive ();
+		Debug.Log ("The Cat is Inactive");
+		CatLock = null;
+		CatInPlace = false;
+}
+}
---
+++ Assets/Scripts/UI Scripts/CameraScript.cs
@@ -0,0 +1,111 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+
+	public MobileInputScript MobileInput;
+	public MobileInputScript MobileInputs;
+
+	public float FOVZoomOut = 60;                     // The field of view for the camera
+	public float FOVZoomIn = 12;
+	public float dragMultiplier = 0.1f;                           // The speed the player can move the camera at
+    public float zoomMultiplier = 0.1f;
+
+	public Vector2 FirstTouch;
+	public Vector2 LastTouch;
+
+
+	public bool CameraMoving;
+
+	public float DragDistance;
+
+	public float CameraLocked = 0;                          // If the camera is Locked it cannot move, Movement speed is set to 0
+	public float CameraUnlocked = 1f;                       // If the camera is unlocked it can now move
+
+	private float lastPinchDistance = -1;
+
+	void Log(string text)
+	{
+		GameObject.Find("Logger").GetComponent<UnityEngine.UI.Text>().text += "\n" + text;
+	}
+	// Use this for initialization
+	void Start () {
+		DragDistance = Screen.height * 20 / 100;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Touch touch = Input.GetTouch (0);
+
+
+
+			
+			if(Input.touchCount == 0)                       // If the player touches the screen with 2 fingers
+			{
+                //Cast a ray, to see if it hits any draggable objects;
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);            // A ray cast is sent from the point on the screen they touched
+                RaycastHit hit;                
+                if (Physics.Raycast(ray, out hit))             
+                {
+                    if (hit.collider.GetComponent<RoomCheckerScript>()) return;                
+                }
+
+				if (MobileInputs.CatPickedUp == true)                       // If the player is picking up a cat they cannot move the camera
+				{
+				dragMultiplier = 0f;
+					Debug.Log ("Cat Picked Up");
+
+				}
+
+
+			if (touch.phase== TouchPhase.Began) 
+			{
+				FirstTouch = touch.position;
+				LastTouch = touch.position;
+			}
+
+			if (touch.phase == TouchPhase.Moved) 
+			{
+				LastTouch = touch.position;
+			}
+
+			if (touch.phase == TouchPhase.Ended) 
+			{
+				LastTouch = touch.position;
+			}
+
+
+			if (Mathf.Abs (LastTouch.x - FirstTouch.x) > DragDistance || Mathf.Abs (LastTouch.y - FirstTouch.y) > DragDistance) 
+			{
+				CameraMoving = true;
+			} 
+			else
+			{
+				CameraMoving = false;
+			}
+
+			if (MobileInput.CatPickedUp == false)                          // When they drop the cat they can move again
+			{
+				dragMultiplier = 0.01f/2;
+				Debug.Log ("Cat Dropped");
+			}
+				//Moving
+				Vector2 dPos = Input.GetTouch(0).deltaPosition;                    // Changes the position of the camera Based on the players finger movemnts
+				//slow the movement
+				dPos *= dragMultiplier;
+
+            if (dPos.y >= 0.5)
+            {
+                transform.position += new Vector3(0, -dPos.y, 0);                  // Updates the Camaras position, The Camera can only move up and down 
+            }
+
+		
+
+			}
+			
+		}
+
+}
+
+
